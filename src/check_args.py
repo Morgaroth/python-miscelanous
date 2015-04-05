6fc48b808f00560071055084ae1d8e9a95736232
Mateusz Jaje <mateuszjaje@gmail.com>
2015-04-05 14:27:39 +0200

    added creating gif script

---
+++ src/check_args.py
@@ -0,0 +1,13 @@
+import argparse
+
+__author__ = 'mateusz'
+
+parser = argparse.ArgumentParser(description='Converts images to smaller size & creates gifs.')
+parser.add_argument('fps', metavar='fpss', type=float, nargs='+',
+                    help='list of frames per second for creating gifs')
+parser.add_argument('-r', '--resize', type=int, metavar='percents',
+                    help='resize ratio in percents to resize images before create gif.')
+parser.add_argument('-s', '--max-size', type=float, metavar='max_size',
+                    help='if passed, images will be resized to rich passed size.')
+args = parser.parse_args()
+print args
