bb76c211f5e9f8e955af879d5a28e929b9c8954c
Morgaroth <mateusz.jaje@gmail.com>
2016-12-09 18:05:33 +0100

    added argparse to gopro renamer script

--- src/rename_gopro_images.py
+++ src/rename_gopro_images.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 # coding=utf-8
+from argparse import ArgumentParser, ArgumentTypeError
+from datetime import timedelta
 from os import rename
 from os.path import exists
 from subprocess import Popen, PIPE
-from sys import argv
+
+
+def check_is_reasonable(value):
+    ivalue = int(value)
+    if ivalue < -12 or ivalue > 11:
+        raise ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+
+
+parser = ArgumentParser(description='Renames GoPro photos to Morgaroth\'s common name format.')
+parser.add_argument('timezone-shift', metavar='shift', type=int, default=0, help='number of hours to shift',
+                    action=check_is_reasonable)
+parser.add_argument('input-files', metavar='input', nargs='+', help='list of files to rename')
+args = parser.parse_args()
 
 
 def exif_date_reader(time_in_str):
@@ -26,13 +41,15 @@ def common_format(date, millis):
         date.year, date.month, date.day, date.hour, date.minute, date.second, millis)
 
 
-files = [f for f in argv[1:] if f.lower().endswith('.jpg')]
+# files = [f for f in argv[1:] if f.lower().endswith('.jpg')]
+files = [f for f in args.input if f.lower().endswith('.jpg')]
 
 grouped = {}
 for f in files:
     exif_dict = get_exif_data(f)
-    date = exif_date_reader(exif_dict['Date/Time Original'])
+    date = exif_date_reader(exif_dict['Date/Time Original']) + timedelta(hours=args.shift)
     if (not f.startswith("GOPR")) or exif_dict['Make'] != 'GoPro' or exif_dict['Camera Model Name'] != 'HERO':
+        print('File {} ignored due to incorrect meta-data, it isn\'t GoPro image'.format(f))
         continue
 
     d = (int(f[4:].lower().rstrip(".jpg")), f)
